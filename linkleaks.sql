763d64cc82839bc4d1cdbe02528bc447c68d27bf
Lana-chen1 <lana.chen@efrei.net>
2026-03-20 23:06:08 +0100

    Add files via upload

---
+++ linkleaks.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS fuite;
+USE fuite;
+
+
+
+CREATE TABLE utilisateur (
+    utilisateur_id INT PRIMARY KEY,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    num_tel varchar(20),
+    adress_mail varchar(100),
+    IP varchar(100)
+);
+
+
+
+CREATE TABLE fuite (
+    fuite_id INT PRIMARY KEY,
+    fuite_latitude varchar(50),
+    fuite_longitude varchar(50)
+);
+
+
+CREATE TABLE signaler (
+	commentaire varchar(50),
+    statut varchar(50),
+    photo varchar(100),
+    utilisateur_id int,
+    fuite_id int,
+    PRIMARY KEY (utilisateur_id, fuite_id),
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(utilisateur_id),
+    FOREIGN KEY (fuite_id) REFERENCES fuite(fuite_id)
+);
+
+
