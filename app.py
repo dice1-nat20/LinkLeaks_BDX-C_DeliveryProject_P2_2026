fcbf955797563dc2588bcdd21724a878529850cf
feiafeifei <lana.chen@efrei.net>
2026-05-21 13:30:23 +0200

    enregistre

--- app.py
+++ app.py
@@ -102,8 +102,5 @@ def success():
 
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=8000, ssl_context='adhoc')
